054e6ee150f62df1381748c3ab35e99fd4883b41
Tony031218 <975062472@qq.com>
2019-08-31 17:26:29 +0800

    19-8-31

--- BIT.py
+++ BIT.py
@@ -7,14 +7,8 @@
 from manimlib.imports import *
 from manim_projects.MyUsefulScene.VideoStart import VideoStart
 
-class VideoTitle(VideoStart):
-    CONFIG = {
-        "title_name": "树状数组($Binary\ Indexed\ Tree$)",
-    }
-
-
-class VideoCover(Scene):
-    def construct(self):
+class TreeScene(Scene):
+    def build(self, low_opacity=False):
         a = list()  #a[1]~a[8] is valid
         a.append(Rectangle(height=0.667, width=1.5).move_to(DOWN*2.667+LEFT*5.25))
         for i in range(1, 9):
@@ -55,15 +49,27 @@ class VideoCover(Scene):
         BIT_tree = VGroup(
             *[a[i] for i in range(1, 9)], *[t[i].set_color(GREEN) for i in range(1, 9)],
             *[textt[i] for i in range(1, 9)], *[texta[i] for i in range(1, 9)],
-            l1_9, l9_10, l10_12, l11_12, l12_16, l13_14, l14_16, l15_16,
+            l9_10, l10_12, l11_12, l12_16, l13_14, l14_16, l15_16,
             l1_9, l2_10, l3_11, l4_12, l5_13, l6_14, l7_15, l8_16
         )
+        if low_opacity:
+            for element in BIT_tree:
+                element.set_stroke(opacity=0.3)
+            for i in range(1, 9):
+                textt[i].set_opacity(0.3)
+                texta[i].set_opacity(0.3)
+        return BIT_tree
 
-        for element in BIT_tree:
-            element.set_stroke(opacity=0.3)
-        for i in range(1, 9):
-            textt[i].set_opacity(0.3)
-            texta[i].set_opacity(0.3)
+
+class VideoTitle(VideoStart):
+    CONFIG = {
+        "title_name": "树状数组($Binary\ Indexed\ Tree$)",
+    }
+
+
+class VideoCover(TreeScene):
+    def construct(self):
+        BIT_tree = self.build(low_opacity=True)
 
         title = TextMobject("树\\ \\ 状\\ \\ 数\\ \\ 组").scale(3).set_color(BLUE).move_to(UP*0.5)
         entitle = TextMobject("\\texttt{Binary Indexed Tree}").scale(1.8).next_to(title, UP).set_color(YELLOW)
@@ -81,8 +87,8 @@ class TableOfContents(Scene):
             TextMobject("引入问题"),
             TextMobject("前置知识\\ -\\ \\texttt{lowbit()}操作"),
             TextMobject("树状数组\\ -\\ 思想及实现"),
-            TextMobject("解决问题"),
             TextMobject("树状数组的扩展应用"),
+            TextMobject("总结"),
         )
         for topic in topics:
             dot = Dot(color=BLUE)
@@ -264,11 +270,156 @@ class PreLowbit(Scene):
         self.wait(5)
 
 
+class BackToProblem(Scene):
+    def construct(self):
+        title = Title("树状数组").set_color(BLUE)
+        problem = VGroup(
+            TextMobject("给出一个长度为$n$的数组,完成以下两种操作"),
+            VGroup(
+                TextMobject("将第$x$个数加上$k$"),
+                TextMobject("输出区间$[x,y]$内每个数的和"),
+            ).arrange_submobjects(
+                DOWN, aligned_edge=LEFT, buff=MED_SMALL_BUFF
+            ).scale(0.8)
+        ).arrange_submobjects(
+            DOWN, aligned_edge=ORIGIN, buff=MED_LARGE_BUFF
+        ).move_to(UP*1.5)
+        for topic in problem[1]:
+            dot = Dot(color=BLUE)
+            dot.next_to(topic, LEFT)
+            topic.add(dot)
+        luogu = TextMobject("题目来源：$Luogu\\ P3374$\\ 树状数组1").scale(0.4).move_to(RIGHT*3.7)
+        self.play(Write(title))
+        self.wait(2)
+        self.play(
+            FadeInFromLarge(problem),
+            FadeInFromLarge(luogu)
+        )
+        self.wait(3)
+
+        mind = VGroup(
+            TextMobject("区间查询").set_color(YELLOW),
+            TextMobject("$\\Longrightarrow$前缀和"),
+            TextMobject("$\\Longrightarrow$","树结构维护","($\\log_2n$)"),
+        ).arrange_submobjects(
+            RIGHT, buff=MED_SMALL_BUFF
+        ).move_to(DOWN)
+        mind[2][1].set_color(RED)
+        self.play(Write(mind[0]))
+        self.wait()
+        self.play(FadeInFrom(mind[1], LEFT))
+        self.wait()
+        self.play(FadeInFrom(mind[2], LEFT))
+        self.wait(2)
+
+
+class BinaryIndexedTree(TreeScene):
+    def construct(self):
+        tree = self.build().scale(0.9).move_to(DOWN*0.4)
+        treepart = VGroup()
+        for i in range(8):
+            self.play(Write(tree[i]), Write(tree[i+24]), run_time=0.2)
+        for i in range(8, 24):
+            treepart.add(tree[i])
+        for i in range(32, 47):
+            treepart.add(tree[i])
+        self.wait(2)
+        self.play(FadeInFromDown(treepart))
+        self.wait(3)
+        dots = VGroup(
+            Dot(color=BLUE).move_to(LEFT*5),
+            Dot(color=BLUE).move_to(RIGHT*5)
+        ).to_edge(UP, buff=LARGE_BUFF)
+        self.play(
+            FadeInFrom(dots[0], LEFT),
+            FadeInFrom(dots[1], RIGHT),
+        )
+
+        title1 = TextMobject("\\texttt{t[x]}保存以$x$为根的子树中叶节点值的和").set_color(BLUE).scale(0.85).to_edge(UP, buff=LARGE_BUFF+DOWN*0.15)
+        self.play(FadeInFrom(title1, UP))
+        self.wait(2)
+        bit = VGroup(
+            TexMobject("000","1").scale(0.8).next_to(tree[16], UP),
+            TexMobject("00","10").scale(0.8).next_to(tree[17], UP),
+            TexMobject("001","1").scale(0.8).next_to(tree[18], UP),
+            TexMobject("0","100").scale(0.8).next_to(tree[19], UP),
+            TexMobject("010","1").scale(0.8).next_to(tree[20], UP),
+            TexMobject("01","10").scale(0.8).next_to(tree[21], UP),
+            TexMobject("011","1").scale(0.8).next_to(tree[22], UP),
+            TexMobject("1000").scale(0.8).next_to(tree[23], UP),
+        )
+        for i in range(8):
+            self.play(ReplacementTransform(tree[i+16][1].copy(), bit[i]), run_time=0.5)
+        self.wait(2)
+        len1 = TexMobject("len=1").scale(0.8).set_color([GREEN,WHITE]).move_to(DOWN*1.6+LEFT*6.2)
+        self.play(
+            ShowCreationThenDestructionAround(bit[0][1]),
+            ShowCreationThenDestructionAround(bit[2][1]),
+            ShowCreationThenDestructionAround(bit[4][1]),
+            ShowCreationThenDestructionAround(bit[6][1]),
+            bit[0][1].set_color, RED,
+            bit[2][1].set_color, RED,
+            bit[4][1].set_color, RED,
+            bit[6][1].set_color, RED,
+        )
+        self.play(
+            ReplacementTransform(VGroup(bit[0][1], bit[2][1], bit[4][1], bit[6][1]).copy(), len1)
+        )
+        self.wait()
+        len2 = TexMobject("len=2").scale(0.8).set_color([GREEN,WHITE]).move_to(DOWN*0.38+LEFT*6.2)
+        self.play(
+            ShowCreationThenDestructionAround(bit[1][1]),
+            ShowCreationThenDestructionAround(bit[5][1]),
+            bit[1][1].set_color, RED,
+            bit[5][1].set_color, RED,
+        )
+        self.play(
+            ReplacementTransform(VGroup(bit[1][1], bit[5][1]).copy(), len2)
+        )
+        self.wait()
+        len4 = TexMobject("len=4").scale(0.8).set_color([GREEN,WHITE]).move_to(UP*0.8+LEFT*6.2)
+        self.play(
+            ShowCreationThenDestructionAround(bit[3][1]),
+            bit[3][1].set_color, RED,
+        )
+        self.play(
+            ReplacementTransform(bit[3][1].copy(), len4)
+        )
+        self.wait()
+        len8 = TexMobject("len=8").scale(0.8).set_color([GREEN,WHITE]).move_to(UP*2.1+LEFT*6.2)
+        self.play(
+            ShowCreationThenDestructionAround(bit[7]),
+            bit[7].set_color, RED,
+        )
+        self.play(
+            ReplacementTransform(bit[7].copy(), len8)
+        )
+        self.wait()
+        title2 = TextMobject("\\texttt{t[x]}节点的长度等于\\texttt{lowbit(x)}").set_color(BLUE).scale(0.85).to_edge(UP, buff=LARGE_BUFF+DOWN*0.15)
+        self.play(
+            FadeOutAndShift(title1, DOWN),
+            FadeInFrom(title2, UP)
+        )
+        self.wait(3)
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 ##------Time Line------##
 # 19.8.26 Have an idea
 # 19.8.27 Write article
-# 19.8.29 Finish 2 main scenes
-# 19.8.30 Finish 1 main scene and video cover
+# 19.8.28 Finish 2 main scenes
+# 19.8.29 Finish 1 main scene and video cover
