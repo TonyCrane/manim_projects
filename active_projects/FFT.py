867b9aafe3678b668834eea051fb268fc4a239d0
Tony031218 <975062472@qq.com>
2020-01-11 12:54:24 +0800

    Finish VideoCover

--- active_projects/FFT.py
+++ active_projects/FFT.py
@@ -15,21 +15,49 @@ class VideoTitle(VideoStart):
 
 class VideoCover(Scene):
     def construct(self):
-        # title  = TextMobject("快\\ 速\\ 傅\\ 里\\ 叶\\ 变\\ 换").scale(2.3).set_color(BLUE).move_to(UP*0.5)
-        # entitle = TextMobject("\\texttt{Fast }","\\texttt{Fourier }", "\\texttt{Transform}").next_to(title, UP).set_color(YELLOW).set_width(title.get_width())
-        # fast1 = TextMobject("\\texttt{Fast }").set_color(GRAY).set_width(entitle[0].get_width()).next_to(entitle[0], UP)
-        # fast2 = TextMobject("\\texttt{Fast }").set_color(GRAY).set_width(entitle[0].get_width()).next_to(entitle[1], UP)
-        # tle   = TextMobject("\\texttt{TLE}").set_color(GRAY).set_height(entitle[0].get_height()).next_to(entitle[2], UP)
-        # xentitle = VGroup(fast1, fast2, tle)
-        # author = TextMobject("@鹤翔万里").set_color([BLUE, YELLOW, ORANGE, RED]).next_to(title, DOWN, buff=1.2)
-        # line = Line(fast1.get_left(), entitle.get_right()+fast1.get_left()-entitle.get_left()).set_color(GRAY)
-        
-        # self.add(title, entitle, author, xentitle, line)
-        self.get_background()
+        # self.add_main()
+        self.add_background()
+        # self.add_subscripts()
 
-    def get_background(self):
+    def add_main(self):
+        title  = TextMobject("快\\ 速\\ 傅\\ 里\\ 叶\\ 变\\ 换").scale(2.3).set_color(BLUE).move_to(UP*0.5)
+        entitle = TextMobject("\\texttt{Fast }","\\texttt{Fourier }", "\\texttt{Transform}").next_to(title, UP).set_color(YELLOW).set_width(title.get_width())
+        fast1 = TextMobject("\\texttt{Fast }").set_color(GRAY).set_width(entitle[0].get_width()).next_to(entitle[0], UP)
+        fast2 = TextMobject("\\texttt{Fast }").set_color(GRAY).set_width(entitle[0].get_width()).next_to(entitle[1], UP)
+        tle   = TextMobject("\\texttt{TLE}").set_color(GRAY).set_height(entitle[0].get_height()).next_to(entitle[2], UP)
+        xentitle = VGroup(fast1, fast2, tle)
+        author = TextMobject("@鹤翔万里").scale(1.2).set_color([BLUE, YELLOW, ORANGE, RED]).next_to(title, DOWN, buff=1.2)
+        line = Line(fast1.get_left(), entitle.get_right()+fast1.get_left()-entitle.get_left()).set_color(GRAY)
+        
+        self.add(title, entitle, author, xentitle, line)
+
+    def add_background(self):
+        pass
+
+    def add_subscripts(self):
+        square = Square().rotate(PI / 4).scale(1.5).set_fill(BLUE, 1).set_color(BLUE)
+        square.move_to(LEFT * 7.3 + UP * 4.6)
+        text = Text("???", font='AR PL KaitiM GB', stroke_width=1.5).scale(0.8).rotate(PI / 4).next_to(square.get_edge_center(DOWN), buff=0)
+        text.shift(UP * 1.3 + RIGHT * 0.24)
+
+        self.add(square, text)
         
 
+class FFTScene(Scene):
+    def construct(self):
+        lines = [[] for i in range(0, 8)]
+
+
+class DotMap(Scene):
+    def construct(self):
+        dots = VGroup()
+        for x in range(-7, 8):
+            for y in range(-4, 5):
+                dots.add(Dot().move_to(RIGHT * x + UP * y))
+        text1 = Text("(-7, -4)", font='Consolas').scale(0.5).move_to(dots[0]).shift(RIGHT * 0.9 + UP * 0.5)
+        text2 = Text("(7, 4)", font='Consolas').scale(0.5).move_to(dots[-1]).shift(DOWN * 0.5 + LEFT * 0.9)
+        self.add(dots, text1, text2)
+                
 
 
 ##------Time Line------##
