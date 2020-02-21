1ac80cca1bec899acc338b3a955f4ddb07d3ae87
Tony031218 <975062472@qq.com>
2020-02-21 22:35:40 +0800

    20-02-21 improve debugTeX from TextMobject to Text

--- tony_useful/debugTeX.py
+++ tony_useful/debugTeX.py
@@ -2,6 +2,6 @@ from manimlib.imports import *
 
 def debugTeX(self, texm):
     for i, j in zip(range(100), texm):
-        tex_id = TextMobject(str(i)).scale(0.3).set_color(PURPLE)
+        tex_id = Text(str(i), font="Consolas").scale(0.1).set_color(PURPLE)
         tex_id.move_to(j)
         self.add(tex_id)
