56bf3f08aa1a8a0a0a56ceb9bee4577d130a3c1d
Tony031218 <975062472@qq.com>
2020-03-03 23:52:02 +0800

    20-03-03 Lissajous

--- active_projects/Lissajous.py
+++ active_projects/Lissajous.py
@@ -223,4 +223,271 @@ class Test3DLissajousFromParametricFunction(ThreeDScene):
         self.begin_ambient_camera_rotation(rate=1)
         self.wait(10)
         
+
+class Intro2DLissajous(Scene):
+    CONFIG = {
+        "camera_config": {
+            "background_color": WHITE
+        }
+    }
+    def construct(self):
+        para = TexMobject("(", "\\cos \\theta", ",", "\\sin 3\\theta", ")").set_color(BLACK)
+        para.scale(1.7).to_corner(DR)
+
+        circle_x = Circle(color=RED)#.shift(UP * 2.5)
+        circle_y = Circle(color=RED)#.shift(LEFT * 2.5)
+
+        self.wait()
+        self.play(ShowCreation(VGroup(circle_x, circle_y)))
+        self.wait()
+        self.play(
+            circle_x.shift, UP * 2.5,
+            circle_y.shift, LEFT * 2.5
+        )
+        self.wait()
         
+        theta = ValueTracker(0)
+        theta_label = TexMobject("\\theta = ").scale(1.7).to_corner(DL).set_color(BLACK)
+        theta_value = DecimalNumber(0, num_decimal_places=2).set_color(BLACK).scale(1.7).next_to(theta_label, RIGHT)
+
+        def updater_of_point_x(obj):
+            obj.move_to(circle_x.point_at_angle(1 * theta.get_value()))
+
+        point_x = Dot(color=GOLD).add_updater(updater_of_point_x)
+        point_y = Dot(color=GOLD).add_updater(lambda m: m.move_to(circle_y.point_at_angle(3 * theta.get_value())))
+
+        line_x = Line(UP * 6  ,  DOWN * 6, stroke_width=2).add_updater(lambda m: m.move_to(point_x.get_center()[0] * RIGHT)).set_color(GRAY).set_opacity(0.6)
+        line_y = Line(LEFT * 8, RIGHT * 8, stroke_width=2).add_updater(lambda m: m.move_to(point_y.get_center()[1] * UP)).set_color(GRAY).set_opacity(0.6)
+
+        self.play(ShowCreation(point_x))
+        self.play(ShowCreation(line_x))
+        self.wait()
+        self.play(theta.increment_value, PI * 2, run_time=5, rate_func=linear)
+        self.play(Write(para[1]))
+
+        theta.set_value(0)
+        point_x.remove_updater(updater_of_point_x)
+
+        self.wait()
+        self.play(ShowCreation(point_y))
+        self.play(ShowCreation(line_y))
+        self.wait()
+        self.play(theta.increment_value, PI * 2, run_time=5, rate_func=linear)
+        self.play(Write(para[3]))
+        self.wait()
+
+        theta.set_value(0)
+        point_x.add_updater(updater_of_point_x)
+
+        P = Dot(color=BLUE).add_updater(lambda m: m.move_to([point_x.get_center()[0], point_y.get_center()[1], 0]))
+
+        path = TracedPath(P.get_center, stroke_width=6, stroke_color=BLUE, min_distance_to_new_point=0.01)
+
+        self.wait()
+        self.play(
+            ShowCreation(P), 
+            FadeIn(VGroup(para[0], para[2], para[4])), 
+            FadeIn(VGroup(theta_label, theta_value))
+        )
+        theta_value.add_updater(lambda m: m.set_value(theta.get_value()))
+        self.add(path)
+        self.wait()
+        self.play(theta.increment_value, PI * 4, run_time=10, rate_func=linear)
+        self.wait(3)
+
+
+class ALotOf2DLissajous(Scene):
+    CONFIG = {
+        "camera_config": {
+            "background_color": WHITE
+        }
+    }
+    def construct(self):
+        w = ValueTracker(1)
+
+        label = VGroup(
+            DecimalNumber(0).set_color(BLACK).scale(2),
+            TexMobject(":").set_color(BLACK).scale(2),
+            TexMobject("3").set_color(BLACK).scale(2)
+        ).arrange(RIGHT)
+        label.to_corner(UL, buff=1)
+        label[0].add_updater(lambda m: m.set_value(w.get_value()))
+
+        line = ParametricFunction(
+            lambda t: [np.cos(1 * t), np.sin(3 * t), 0],
+            t_min=0, t_max=6*TAU, color=BLUE
+        )
+        
+        def updater_of_line(obj):
+            new = ParametricFunction(
+                lambda t: [np.cos(w.get_value() * t), np.sin(3 * t), 0],
+                t_min=0, t_max=6*TAU, color=BLUE
+            )
+            obj.become(new)
+        
+        self.play(FadeIn(line), FadeIn(label))
+        self.wait()
+        for i in range(6):
+            line.add_updater(updater_of_line)
+            self.play(w.increment_value, 1, run_time=1, rate_func=linear)
+            line.remove_updater(updater_of_line)
+            self.wait()
+        self.wait()
+        line.add_updater(updater_of_line)
+        self.play(w.set_value, 1)
+        line.remove_updater(updater_of_line)
+        self.wait(2)
+
+
+def smooth2(t, inflection=6):
+    error = sigmoid(-inflection / 2)
+    return np.clip(
+        (sigmoid(inflection * (t - 0.5)) - error) / (1 - 2 * error),
+        0, 1,
+    )
+
+class From2DTo3DLissajous(ThreeDScene):
+    CONFIG = {
+        "camera_config": {
+            "background_color": WHITE
+        }
+    }
+    def construct(self):
+        axes = ThreeDAxes(number_line_config={"include_tip": False})
+        self.set_camera_orientation(phi=0*DEGREES, theta=90*DEGREES, distance=10000)
+        line = ParametricFunction(
+            lambda t: np.array([np.cos(1 * t), np.sin(3 * t), np.sin(2 * t)]),
+            t_min=0, t_max=4 * TAU, color=BLUE
+        )
+        self.add(line)
+        self.wait()
+        self.play(FadeIn(axes))
+        self.wait()
+        self.move_camera(phi=70*DEGREES, theta=135*DEGREES, rate_func=smooth2, run_time=5)
+        self.begin_ambient_camera_rotation(rate=0.8)
+        self.wait(10)
+        self.play(FadeOut(axes), FadeOut(line))
+        self.wait()
+
+
+class ReadyTo3DLissajous(ThreeDScene):
+    CONFIG = {
+        "camera_config": {
+            "background_color": WHITE
+        },
+        "dot_class": Dot3D,
+        "plane_use_ploygon": False,
+        "line_class": Line,
+    }
+    def construct(self):
+        axes = ThreeDAxes()
+        # self.add(axes)
+        self.set_camera_orientation(phi=70*DEGREES, theta=45*DEGREES)
+        # self.set_camera_orientation(distance=1000000)
+        self.begin_ambient_camera_rotation(rate=0.5)
+
+        circle_x = Circle(color=RED).rotate(PI / 2, RIGHT).shift(DOWN * 2.5).set_shade_in_3d()
+        circle_y = Circle(color=RED).rotate(PI / 2, DOWN).shift(LEFT * 2.5).set_shade_in_3d()
+        circle_z = Circle(color=RED).shift(IN * 2.5).set_shade_in_3d()
+
+        self.wait()
+        self.play(
+            ShowCreation(VGroup(circle_x, circle_y, circle_z))
+        )
+
+        theta = ValueTracker(0)
+
+        def updater_of_point_x(m):
+            m.move_to(circle_x.point_at_angle(1 * theta.get_value()))
+        def updater_of_point_y(m):
+            m.move_to(circle_y.point_at_angle(2 * theta.get_value()))
+        def updater_of_point_z(m):
+            m.move_to(circle_z.point_at_angle(3 * theta.get_value()))
+
+        def updater_of_plane_x(m):
+            lambda m: m.move_to(point_x.get_center()[2] * OUT)
+        def updater_of_plane_y(m):
+            lambda m: m.move_to(point_y.get_center()[1] * UP)
+        def updater_of_plane_z(m):
+            lambda m: m.move_to(point_z.get_center()[0] * RIGHT)
+
+        point_x = self.dot_class().add_updater(updater_of_point_x).set_color(GREEN)
+        plane_x = ParametricSurface(
+            lambda u, v: np.array([u, v, 0]),
+            u_min=-2.5, u_max=2.5, v_min=-2.5, v_max=2.5,
+            checkerboard_colors=None,
+            fill_color=GREEN, fill_opacity=0.3,
+            stroke_width=0
+        ).add_updater(updater_of_plane_x)
+        
+        self.wait()
+        self.play(ShowCreation(point_x))
+        self.play(ShowCreation(plane_x))
+        self.wait()
+        self.play(theta.increment_value, PI*2, run_time=5, rate_func=linear)
+        self.wait()
+        point_x.remove_updater(updater_of_point_x)
+        plane_x.add_updater(updater_of_plane_x)
+
+        point_y = self.dot_class().add_updater(updater_of_point_y).set_color(ORANGE)
+        plane_y = ParametricSurface(
+            lambda u, v: np.array([u, 0, v]),
+            u_min=-2.5, u_max=2.5, v_min=-2.5, v_max=2.5,
+            checkerboard_colors=None,
+            fill_color=ORANGE, fill_opacity=0.3,
+            stroke_width=0
+        ).add_updater(updater_of_plane_y)
+
+        self.wait()
+        self.play(ShowCreation(point_y))
+        self.play(ShowCreation(plane_y))
+        self.wait()
+        self.play(theta.increment_value, PI*2, run_time=5, rate_func=linear)
+        self.wait()
+        point_y.remove_updater(updater_of_point_y)
+        plane_y.add_updater(updater_of_plane_y)
+        theta.set_value(0)
+
+        point_z = self.dot_class().set_color(PURPLE)
+        plane_z = ParametricSurface(
+            lambda u, v: np.array([0, u, v]),
+            u_min=-2.5, u_max=2.5, v_min=-2.5, v_max=2.5,
+            checkerboard_colors=None,
+            fill_color=PURPLE, fill_opacity=0.3,
+            stroke_width=0
+        ).add_updater(updater_of_plane_z)
+
+        self.wait()
+        self.play(ShowCreation(point_z))
+        self.play(ShowCreation(plane_z))
+        self.wait()
+        self.play(theta.increment_value, PI*2, run_time=5, rate_func=linear)
+        self.wait()
+        theta.set_value(0)
+
+        line_x = self.line_class(
+            np.array([-2.5, 0, 0]),
+            np.array([ 2.5, 0, 0]),
+            fill_color=GOLD_E,
+            stroke_width=2
+        ).add_updater(lambda m: m.move_to(point_y.get_center()[1] * UP + point_x.get_center()[2] * OUT))
+        line_y = self.line_class(
+            np.array([0, -2.5, 0]),
+            np.array([0,  2.5, 0]),
+            fill_color=GOLD_E,
+            stroke_width=2
+        ).add_updater(lambda m: m.move_to(point_z.get_center()[0] * RIGHT + point_x.get_center()[2] * OUT))
+        line_z = self.line_class(
+            np.array([0, 0, -2.5]),
+            np.array([0, 0,  2.5]),
+            fill_color=GOLD_E,
+            stroke_width=2
+        ).add_updater(lambda m: m.move_to(point_z.get_center()[0] * RIGHT + point_y.get_center()[1] * UP))
+
+        P = self.dot_class().set_shade_in_3d(False)
+        P.add_updater(lambda m: m.move_to(np.array([point_z.get_center()[0], point_y.get_center()[1], point_x.get_center()[2]])))
+
+        self.wait()
+        self.add(line_x, line_y, line_z, P)
+        self.wait()
+        self.move_camera(phi=70*DEGREES, theta=45*DEGREES)
