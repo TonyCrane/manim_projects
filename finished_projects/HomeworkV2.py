6659f07df2f2f65ce9b4afe50b01865d7479cdf3
Tony031218 <975062472@qq.com>
2020-03-28 11:56:54 +0800

    20-03-28 finish homework 03

--- finished_projects/HomeworkV2.py
+++ finished_projects/HomeworkV2.py
@@ -2,7 +2,7 @@ from manimlib.imports import *
 from manim_projects.tony_useful.imports import *
 from manim_sandbox.utils.imports import *
 
-class Homework_01(Scene):
+class Homework_02(Scene):
     def construct(self):
         self.intro_problem()
         self.main_proof()
