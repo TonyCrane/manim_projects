7be91cc4dc896986b5be679c5e526eafc34589ba
Tony031218 <975062472@qq.com>
2020-05-24 08:54:31 +0800

    Update debugTeX

--- tony_useful/debugTeX.py
+++ tony_useful/debugTeX.py
@@ -1,7 +1,7 @@
 from manimlib.imports import *
 import itertools
 
-def debugTeX(self, texm, scale=0.3):
+def debugTeX(self, texm, scale=0.6):
     for i, j in enumerate(texm):
         tex_id = Text(str(i), font="Consolas").scale(scale).set_color(PURPLE)
         tex_id.move_to(j)
